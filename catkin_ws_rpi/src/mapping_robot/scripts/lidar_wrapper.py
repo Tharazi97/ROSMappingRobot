46c91ea20bbb8d46a73d38ffd4b1e5f31daf1f31
MichalJes <michal.jes@gmail.com>
2019-10-08 23:16:41 +0200

    Add scripts integrated into ROS files
    -> Adding source.txt that contains source pages used to develope scripts
    -> Updated GitIgnore file
    -> Integrated functions into lidar_wrapper script
    -> Deleted extra files

--- catkin_ws_rpi/src/mapping_robot/scripts/lidar_wrapper.py
+++ catkin_ws_rpi/src/mapping_robot/scripts/lidar_wrapper.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
 
+import sys
+import time
+import wiringpi
+import serial
 import rospy
 from std_msgs.msg import Bool
 from mapping_robot.msg import Point
 
-# We are using this subscriber publiher class to ease using publish and subscribe at the same time
+lid = serial.Serial(port=None,
+					baudrate=115200,
+					bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE,
+					stopbits=serial.STOPBITS_ONE,
+					timeout=2)
+
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+
+# set #18 to be a PWM output
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.pinMode(12, wiringpi.GPIO.PWM_OUTPUT)
+
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+# divide down clock
+wiringpi.pwmSetClock(192)  # 50Hz servo
+wiringpi.pwmSetRange(2000)
+# We are using this subscriber publisher class to ease using publish and subscribe at the same time
 class SubPub:
 	# In the callback function we invoke all peripherals wrapper functions
 	def callback(self, msg):
@@ -21,6 +45,22 @@ class SubPub:
 					#point.yaw = angle.yaw
 					self.pub.publish(point)
 					#move_servo(i, j)
+
+	# def lidar_read(self):
+	# 	measure_dist = [90, 4, 4, 98]
+	# 	delay_period = 0.1
+	# 	lid.write(measure_dist)
+	# 	time.sleep(delay_period)
+	# 	serial_data = lid.read(size=9)
+	# 	if int.from_bytes(serial_data(1), byteorder='big') == 89 and int.from_bytes(serial_data(2), byteorder='big') == 89:
+	# 		dist_H = serial_data(4)
+	# 		dist_L = serial_data(3)
+	# 		dist = dist_H + dist_L
+	# 		return(dist)
+
+	def move_servo(self,i, j):
+		wiringpi.pwmWrite(18, i*(5/9))
+		wiringpi.pwmWrite(12, j*(5/9))	
 
 	def __init__(self):	
 		# Inform master that we will be subscribing Bool msgs on the create_map toppic
