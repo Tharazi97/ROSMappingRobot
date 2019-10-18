7d0c795c3dce516b266b1626d43bd417d0d8f0b9
Tharazi97 <kwiatkowski.szymon97@gmail.com>
2019-10-18 16:46:36 +0200

    Working lidar

--- catkin_ws_rpi/src/mapping_robot/scripts/lidar_wrapper.py
+++ catkin_ws_rpi/src/mapping_robot/scripts/lidar_wrapper.py
@@ -2,6 +2,7 @@
 
 import rospy
 import wiringpi
+import serial
 from std_msgs.msg import Bool
 from mapping_robot.msg import Point
 
@@ -18,13 +19,27 @@ wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
 wiringpi.pwmSetClock(96)
 wiringpi.pwmSetRange(4000)
 
+lid = serial.Serial(port = "/dev/ttyAMA0", baudrate = 115200, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, timeout = 2)
+
 # We are using this subscriber publiher class to ease using publish and subscribe at the same time
 class SubPub:
     def move_servo(self, i, j):
         # We need to convert degrees to the value between 0-4000
         wiringpi.pwmWrite(12, 108+(i*372/180))
         wiringpi.pwmWrite(18, 108+(j*372/180))
-        rospy.sleep(0.1)
+        rospy.sleep(1)
+
+    def lidar_read(self):
+        	sum = 0
+	for i in range(10):
+	        lid.write(bytearray([90, 4, 4, 98]))
+        	# Store read data in list format to have easer acces to separate bytes
+		serial_data = list(lid.read(9))
+		# Check frame start
+        	if serial_data[0] == 'Y' and serial_data[1] == 'Y':
+			# Third byte is distance L_byte and fourth byte is distance H_byte, so it needs to by shifted by 8
+			sum += ord(serial_data[2]) + (ord(serial_data[3]) << 8)
+	return (sum/10)
 
     # In the callback function we invoke all peripherals wrapper functions
     def callback(self, msg):
@@ -32,9 +47,8 @@ class SubPub:
             for i in range(180):
                 for j in range(180):
                     point = Point()
-                    point.distance = i + j
+                    point.distance = self.lidar_read()
                     # TODO: Michals peripherals functions
-                    #point.distance = lidar_read()
                     #angle = mpu_read()
                     #point.roll = angle.roll
                     #point.pitch = angle.pitch
