1db6c242f6552f1f348d826c4455bf8749751a5c
Szymon Kwiatkowski <44876530+Tharazi97@users.noreply.github.com>
2019-10-22 10:14:31 +0200

    Testing lidar (#6)
    * Working PWM
    
    * Working lidar

--- catkin_ws_rpi/src/mapping_robot/scripts/lidar_wrapper.py
+++ catkin_ws_rpi/src/mapping_robot/scripts/lidar_wrapper.py
@@ -1,75 +1,70 @@
 #!/usr/bin/env python
 
-import sys
-import time
+import rospy
 import wiringpi
 import serial
-import rospy
 from std_msgs.msg import Bool
 from mapping_robot.msg import Point
 
-lid = serial.Serial(port=None,
-					baudrate=115200,
-					bytesize=serial.EIGHTBITS,
-                    parity=serial.PARITY_NONE,
-					stopbits=serial.STOPBITS_ONE,
-					timeout=2)
-
-# use 'GPIO naming'
 wiringpi.wiringPiSetupGpio()
 
-# set #18 to be a PWM output
-wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+# Set GPIO18 as horizontal servo and GPIO12 as vertical servo
 wiringpi.pinMode(12, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
 
-# set the PWM mode to milliseconds stype
+# Set PWM to mark:space mode
 wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
 
-# divide down clock
-wiringpi.pwmSetClock(192)  # 50Hz servo
-wiringpi.pwmSetRange(2000)
-# We are using this subscriber publisher class to ease using publish and subscribe at the same time
+# Set the dvider of PWM clock to 96 and range of counting to 4000 so it gives 50 Hz frequency
+wiringpi.pwmSetClock(96)
+wiringpi.pwmSetRange(4000)
+
+lid = serial.Serial(port = "/dev/ttyAMA0", baudrate = 115200, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, timeout = 2)
+
+# We are using this subscriber publiher class to ease using publish and subscribe at the same time
 class SubPub:
-	# In the callback function we invoke all peripherals wrapper functions
-	def callback(self, msg):
-		if msg.data == 1:
-			for i in range(180):
-				for j in range(180):
-					point = Point()
-					point.distance = i + j
-					# TODO: Michals peripherals functions
-					#point.distance = lidar_read()
-					#angle = mpu_read()
-					#point.roll = angle.roll
-					#point.pitch = angle.pitch
-					#point.yaw = angle.yaw
-					self.pub.publish(point)
-					#move_servo(i, j)
+    def move_servo(self, i, j):
+        # We need to convert degrees to the value between 0-4000
+        wiringpi.pwmWrite(12, 108+(i*372/180))
+        wiringpi.pwmWrite(18, 108+(j*372/180))
+        rospy.sleep(1)
 
-	# def lidar_read(self):
-	# 	measure_dist = [90, 4, 4, 98]
-	# 	delay_period = 0.1
-	# 	lid.write(measure_dist)
-	# 	time.sleep(delay_period)
-	# 	serial_data = lid.read(size=9)
-	# 	if int.from_bytes(serial_data(1), byteorder='big') == 89 and int.from_bytes(serial_data(2), byteorder='big') == 89:
-	# 		dist_H = serial_data(4)
-	# 		dist_L = serial_data(3)
-	# 		dist = dist_H + dist_L
-	# 		return(dist)
+    def lidar_read(self):
+        	sum = 0
+	for i in range(10):
+	        lid.write(bytearray([90, 4, 4, 98]))
+        	# Store read data in list format to have easer acces to separate bytes
+		serial_data = list(lid.read(9))
+		# Check frame start
+        	if serial_data[0] == 'Y' and serial_data[1] == 'Y':
+			# Third byte is distance L_byte and fourth byte is distance H_byte, so it needs to by shifted by 8
+			sum += ord(serial_data[2]) + (ord(serial_data[3]) << 8)
+	return (sum/10)
 
-	def move_servo(self,i, j):
-		wiringpi.pwmWrite(18, i*(5/9))
-		wiringpi.pwmWrite(12, j*(5/9))	
+    # In the callback function we invoke all peripherals wrapper functions
+    def callback(self, msg):
+        if msg.data == 1:
+            for i in range(180):
+                for j in range(180):
+                    point = Point()
+                    point.distance = self.lidar_read()
+                    # TODO: Michals peripherals functions
+                    #angle = mpu_read()
+                    #point.roll = angle.roll
+                    #point.pitch = angle.pitch
+                    #point.yaw = angle.yaw
+                    self.pub.publish(point)
+                    self.move_servo(j, j)
 
-	def __init__(self):	
-		# Inform master that we will be subscribing Bool msgs on the create_map toppic
-		self.sub = rospy.Subscriber('create_map', Bool, self.callback)
+    def __init__(self):	
+        # Inform master that we will be subscribing Bool msgs on the create_map toppic
+        self.sub = rospy.Subscriber('create_map', Bool, self.callback)
 
-		# Inform master that we will be publishing Point msgs on the measured_points toppic.
-		self.pub = rospy.Publisher("measured_points", Point, queue_size=100)
+        # Inform master that we will be publishing Point msgs on the measured_points toppic.
+        self.pub = rospy.Publisher("measured_points", Point, queue_size=100)
+
 
 if __name__ == '__main__':
-	rospy.init_node('lidar_wrapper', anonymous=True)
+	rospy.init_node('lidar_wrapper', anonymous=False)
 	handle = SubPub()
 	rospy.spin()
